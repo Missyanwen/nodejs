1040a5d5225aa480033c5926fe29c66d7561732d
yanwen <yanwen408431293@163.com>
2017-02-12 16:39:22 +0800

    readme 说明
    Signed-off-by: yanwen <yanwen408431293@163.com>

--- README.md
+++ README.md
@@ -11,12 +11,11 @@
       - app.js 主入口文件  
 
 
-```
-    这里少了一个db目录是存放数据库存储的，需求去mongodb官网下载适合自己机器的安装 (https://www.mongodb.com/download-center?jmp=nav#community)
+
+    这里少了一个db目录是存放数据库存储的，需求去[官网](https://www.mongodb.com/download-center?jmp=nav#community)下载适合自己机器的安装 
     把他的安装放到一个mongodb目录下，需要用到他bin下的mongod.exe
-    打开cmd命令行找到mongodb目录下的bin目录运行 [mongod --dbpath=D:\project\nodejs\db --port=27017]
+    打开cmd命令行找到mongodb目录下的bin目录运行 [mongod --dbpath=D:\project\nodejs\db --port=27017]启动数据库
     --dbpath是指定到那个目录就是上面的db目录
     --port是端口号它默认是27017
 
-    当然建议去下载他的可视化工具(https://robomongo.org/)
-```
+    然后去[下载](https://robomongo.org/)他的可视化工具,进行表结构构建
--- public/js/index.js
+++ public/js/index.js
@@ -13,20 +13,20 @@ $(function(){
 		$registerBox.hide()
 	})
   	
-  	$registerBox.find('button').on('click',function(){
-  		//通过 ajax提交请求
-  		$.ajax({
-  			type:'post',
-  			url:'/api/user/register',
-  			data:{
-  				username:$registerBox.find('[name="username"]').val(),
-  				password:$registerBox.find('[name="password"]').val(),
-  				repassword:$registerBox.find('[name="repassword"]').val()
-  			},
-  			dataType:'json',
-  			success:function(result) {
-  				console.log(result);
-  			}	
-  		})
-  	})
+	$registerBox.find('button').on('click',function(){
+		//通过 ajax提交请求
+		$.ajax({
+			type:'post',
+			url:'/api/user/register',
+			data:{
+				username:$registerBox.find('[name="username"]').val(),
+				password:$registerBox.find('[name="password"]').val(),
+				repassword:$registerBox.find('[name="repassword"]').val()
+			},
+			dataType:'json',
+			success:function(result) {
+				console.log(result);
+			}	
+		})
+	})
 })
